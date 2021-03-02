4c5a384983fcabb3867defafc095280c0c77bc1b
Martelly <kulbanikolai@yandex.ru>
2021-03-02 18:27:17 +0300

    Added SQL index for offer model

--- models/offer.go
+++ models/offer.go
@@ -5,8 +5,8 @@ import (
 )
 
 type Offer struct {
-	OfferId   uint64    `gorm:"primaryKey;autoIncrement:false;index" json:"offer_id"`
-	SellerId  uint64    `gorm:"primaryKey;autoIncrement:false;index" json:"seller_id"`
+	OfferId   uint64    `gorm:"primaryKey;autoIncrement:false;index:idx_offer" json:"offer_id"`
+	SellerId  uint64    `gorm:"primaryKey;autoIncrement:false;index:idx_offer" json:"seller_id"`
 	CreatedAt time.Time `json:"-"`
 	UpdatedAt time.Time `json:"-"`
 	Name      string    `json:"name"`
